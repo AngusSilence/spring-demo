e325f43ef913c8c9b6b35345650347f242bc62b9
levinzhang1981 <levinzhang1981@gmail.com>
2021-04-14 20:37:23 +0800

    Spring Reactive

---
+++ spring-reatcive/Spring/reactive-redis/src/main/java/com/levinzhang/reactive/redis/SpringReactiveRedisApplication.java
@@ -0,0 +1,13 @@
+package com.levinzhang.reactive.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveRedisApplication.class, args);
+    }
+
+}
