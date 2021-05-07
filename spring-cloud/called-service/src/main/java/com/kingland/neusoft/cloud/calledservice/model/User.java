4e19a41d5f2a21793d63a3681213b8b55e161fa7
levinzhang1981 <levinzhang1981@gmail.com>
2021-05-07 08:16:15 +0800

    Spring Cloud

---
+++ spring-cloud/called-service/src/main/java/com/kingland/neusoft/cloud/calledservice/model/User.java
@@ -0,0 +1,72 @@
+package com.kingland.neusoft.cloud.calledservice.model;
+
+
+import javax.validation.constraints.*;
+
+public class User {
+    @Min(1)
+    public int id;
+    @NotNull
+    @Max(120)
+    @Min(0)
+    public int age;
+    @NotBlank
+    @Size(max = 20, min = 3)
+    public String name;
+    @Email
+    public String emailAddress;
+    @Pattern(regexp = "(^[1-9]\\d{5}(19|([23]\\d))\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$)|(^[1-9]\\d{5}\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{2}[0-9Xx]$)")
+    public String idNo;
+
+    public User() {
+    }
+
+    public User(int id, @NotNull @Max(120) @Min(0) int age, @NotBlank @Size(max = 20, min = 5) String name,
+                @Email String emailAddress, String idNo) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.idNo = idNo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+}
